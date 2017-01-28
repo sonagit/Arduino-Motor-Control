d2e3ea911f4925170b2f744f6037c72103ab3263
Stu <supadupastu@gmail.com>
2017-01-27 22:33:41 -0800

    PD Control

--- Lab3_PD_Control.ino
+++ Lab3_PD_Control.ino
@@ -2,12 +2,12 @@
 MAE 156A PD control of motor
 Motor Control and encoder sampling
 */
-#define DIRpin = 8;
-#define PWMpin = 9;
-#define encPinA = 2;
-#define encPinB = 3;
+#define DIRpin  8
+#define PWMpin  9
+#define encPinA 2
+#define encPinB 3
 
-int deg; // potentiometer position
+int theta; // potentiometer position
 int sampleTime = 20; // time step
 int setpoint; // desired position
 int error; // difference between position and setpoint
@@ -15,33 +15,39 @@ int prevError; // previous sampling error (for Derv control)
 
 long prevTime = millis(); // time at previous sample
 long prevPrintTime = millis(); // time at prev print
-float Kp=10; // proportional control const
+float Kp=0; // proportional control const
 
-///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void setup()
 {
   Serial.begin(9600);
-  // give user 10 seconds to type constants
-  Serial.setTimeout(10000);
+  // give user time to type constants
+  Serial.setTimeout(3000);
 
   pinMode(PWMpin, OUTPUT);
   pinMode(DIRpin, OUTPUT);
 
+  // get current position
+  theta = map(analogRead(A3),0,1024,0,340);
+
   // set desired postion to 180 degrees past initial position
-  setpoint = map(analogRead(A3),0,1024,0,340) + 180;
+  setpoint = theta + 180;
 
   // correct setpoint if outside 0-340 degrees
   if (setpoint>340){setpoint = setpoint-340;}
 
   // Ask for Kp value
-  //Serial.print("Enter an integer for Kp: ");
-  if(Serial.available()>0){
-    Kp = Serial.readStringUntil('\n').toFloat();
+  while (Kp==0)
+  {
+    Serial.print("Enter an integer for Kp: ");
+    Kp = (float) Serial.readStringUntil('\n').toFloat();
+    Serial.println();
   }
+  Serial.print("Kp = ");
+  Serial.println(Kp);
 
-  Serial.println();
   Serial.print("Current position: ");
-  Serial.print(map(analogRead(A3),0,1024,0,340));
+  Serial.print(theta);
   Serial.println(" Degrees");
   Serial.print("Desired Position: ");
   Serial.print(setpoint);
@@ -50,16 +56,16 @@ void setup()
   delay(2000);
 }
 
-/////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void loop(){
 
   //controller
   if(millis()-prevTime>sampleTime){
     // get current position
-    deg = map(analogRead(A3),0,1024,0,340);
+    theta = map(analogRead(A3),0,1024,0,340);
 
     // calc error
-    error = deg-setpoint;
+    error = setpoint-theta;
 
     // set motor output
     setMotor(Kp*error);
@@ -71,23 +77,35 @@ void loop(){
 
   // read pot, send to serial slower than controller
   if(millis()-prevPrintTime>sampleTime*10){
-    Serial.println(deg);
+    Serial.print(setpoint);
+    Serial.print(' ');
+    Serial.print(theta);
+    Serial.print(' ');
+    Serial.println(error);
     prevPrintTime = millis();
   }
 }
-
-void setMotor(int motorSpeed){
+///////////////////////////////////////////////////////////////////////////////
+// setMotor
+// Function to set speed and direction of motor.
+// Has saturation protection
+///////////////////////////////////////////////////////////////////////////////
+int setMotor(int motorSpeed)
+{
   // Set motor direction
   if (motorSpeed>0){digitalWrite(DIRpin,HIGH);}
   else{digitalWrite(DIRpin,LOW);}
 
-  // Set motor speed
+  // Saturation protection
   if (motorSpeed>100){motorSpeed=100;}
   if (motorSpeed<-100){motorSpeed=-100;}
 
-  // convert 0-100% to 0-255 duty cycle
-  int motorDuty = map(motorSpeed,0,100,0,255);
+  // Return the actual controller output with saturation protection
+  return motorSpeed;
 
+  // convert 0-100% to 0-255 duty cycle
+  int motorDuty = map(abs(motorSpeed),0,100,0,255);
+  
   // send PWM to motor
   analogWrite(PWMpin,motorDuty);
 }
